bd4e570f6c52c70b54f347b710f390c4c1397a36
Daniel Ramos <danielramosacosta1@gmail.com>
2023-06-19 20:32:56 +0100

    try to fix the audio

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
-import path from "path";
-const isGitHubPages = true;
-const folderName = path.basename(process.cwd()) + "/";
 const mode = process.env.NODE_ENV === "production" ? "production" : "development";
-const base = mode === "production" && isGitHubPages ? "/" + folderName : "/";
 
 export default {
   root: "src",
-  base,
+  base: "/AquaGout-Game",
   mode,
   publicDir: "../public",
   build: {
